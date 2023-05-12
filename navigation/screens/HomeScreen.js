c44423139543d59558807488158092c04dc3c386
Ayspeed <chebbi41.bilel@hotmail.fr>
2023-05-12 19:19:22 +0200

    homeScreen v1.0

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,12 +1,79 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons'; // installation du package vector-icons
 
-export default function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
-        </View>
-    );
-}
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome</Text>
+      <Text style={styles.description}>Introduction & description</Text>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Nous rejoindre</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Se connecter</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.socialMediaButtonsContainer}>
+        <TouchableOpacity style={styles.socialMediaButton}>
+          <FontAwesome name="facebook" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialMediaButton}>
+          <FontAwesome name="instagram" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialMediaButton}>
+          <FontAwesome name="twitter" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  socialMediaButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  socialMediaButton: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 50,
+    marginHorizontal: 10,
+  },
+});
+
+export default HomeScreen;
