783c8dc968d9c222cfd1470bb0190d8d96c3d123
Ayspeed <chebbi41.bilel@hotmail.fr>
2023-05-24 17:10:23 +0200

    ajout header menu

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,8 +1,11 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { MenuProvider, Menu, MenuTrigger, MenuOptions, MenuOption } from 'react-native-popup-menu';
 
 // Screens
 import HomeScreen from './screens/HomeScreen';
@@ -13,67 +16,89 @@ import AboutScreen from './screens/AboutScreen';
 import LegalScreen from './screens/LegalScreen';
 import PrivacyScreen from './screens/PrivacyScreen';
 
-//Screen names
+// Screen names
 const homeName = "Accueil";
 const calendarName = "Calendrier";
 const notificationsName = "Notifications";
 const profileName = "Profil";
 const aboutName = "A propos";
 const legalName = "Mentions légales";
-const privacyScreen = "Politique de confidentialité";
+const privacyName = "Politique de confidentialité";
 
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function MainContainer() {
-  
-return (
-  
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={homeName}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            let rn = route.name;
-
-            if (rn === homeName) {
-              iconName = focused ? 'home' : 'home-outline';
-
-            } else if (rn === calendarName) {
-              iconName = focused ? 'calendar' : 'calendar-outline';
-
-            } else if (rn === notificationsName) {
-              iconName = focused ? 'notifications' : 'notifications-outline';
-
-            } else if (rn === profileName) {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'grey',
-          tabBarLabelStyle: { paddingBottom: 10, fontSize: 10 },
-          tabBarStyle: { padding: 10, height: 70}
-        })}
-      >
-        <Tab.Screen name={homeName} component={HomeScreen} options={({ navigation }) => ({
-          headerTitleAlign: 'center'
-        })} />
-        <Tab.Screen name={calendarName} component={CalendarScreen} options={({ navigation }) => ({
-          headerTitleAlign: 'center'
-        })} />
-        <Tab.Screen name={notificationsName} component={NotificationsScreen} options={({ navigation }) => ({
-          headerTitleAlign: 'center'
-        })} />
-        <Tab.Screen name={profileName} component={ProfileScreen} options={({ navigation }) => ({
-        headerTitleAlign: 'center'
-      })} />
-      </Tab.Navigator>
-    </NavigationContainer>
-
+  return (
+    <MenuProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Tabs" component={TabNavigator} options={{ headerShown: false }} />
+          <Stack.Screen name={aboutName} component={AboutScreen} />
+          <Stack.Screen name={legalName} component={LegalScreen} />
+          <Stack.Screen name={privacyName} component={PrivacyScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MenuProvider>
   );
 }
+
+function TabNavigator() {
+  const navigation = useNavigation();
+
+  const renderScreenOptions = ({ route }) => {
+    const iconName = route.name === homeName ? 'home' :
+      route.name === calendarName ? 'calendar' :
+      route.name === notificationsName ? 'notifications' :
+      route.name === profileName ? 'person' : '';
+
+    return {
+      tabBarIcon: ({ focused, color, size }) => (
+        <Ionicons name={focused ? iconName : iconName + '-outline'} size={size} color={color} />
+      ),
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'grey',
+      tabBarLabelStyle: { paddingBottom: 10, fontSize: 10 },
+      tabBarStyle: { padding: 10, height: 70 },
+      headerTitleAlign: 'center',
+      headerRight: () => renderMenu(navigation, route)
+    };
+  };
+
+  const renderMenu = (navigation, route) => {
+    return (
+      <View style={styles.menuContainer}>
+        <Menu>
+          <MenuTrigger>
+            <Ionicons name="ellipsis-vertical" size={24} color="black" />
+          </MenuTrigger>
+          <MenuOptions>
+            <MenuOption onSelect={() => navigation.navigate(aboutName)} text="A propos" />
+            <MenuOption onSelect={() => navigation.navigate(legalName)} text="Mentions légales" />
+            <MenuOption onSelect={() => navigation.navigate(privacyName)} text="Politique de confidentialité" />
+          </MenuOptions>
+        </Menu>
+      </View>
+    );
+  };
+
+  return (
+    <Tab.Navigator
+      initialRouteName={homeName}
+      screenOptions={renderScreenOptions}
+    >
+      <Tab.Screen name={homeName} component={HomeScreen} />
+      <Tab.Screen name={calendarName} component={CalendarScreen} />
+      <Tab.Screen name={notificationsName} component={NotificationsScreen} />
+      <Tab.Screen name={profileName} component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  menuContainer: {
+    marginRight: 10,
+  },
+});
 
 export default MainContainer;
